7c8436fc50720b73b82e0dc6f9f99f4d9ce6441b
yurakushniruk <yurakushniruk@ukr.net>
2020-09-08 20:21:05 +0300

    Initial commit

---
+++ Controllers/RootController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public ActionResult GetRoot()
+        {
+            var response = new 
+            { 
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                }
+                    
+            };
+
+            return Ok(response);
+        }
+    }
+}
