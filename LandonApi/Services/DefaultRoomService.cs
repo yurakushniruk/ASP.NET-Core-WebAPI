a2ceb521b82fba10302d48906f347f12a9e58765
yurakushniruk <yurakushniruk@ukr.net>
2020-09-21 21:54:21 +0300

    Several minor changes

---
+++ LandonApi/Services/DefaultRoomService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using LandonApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Services
+{
+    public class DefaultRoomService : IRoomService
+    {
+        private readonly HotelApiDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DefaultRoomService(HotelApiDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Room> GetRoomAsync(Guid id)
+        {
+            var entity = await _context.Rooms.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<Room>(entity);
+        }
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LandonApi.Filters;
+using LandonApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using LandonApi.Services;
+using AutoMapper;
+using LandonApi.Infrastructure;
 
 namespace LandonApi
 {
@@ -27,10 +33,20 @@ namespace LandonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<HotelInfo>(Configuration.GetSection("Info"));
+
+            services.AddScoped<IRoomService, DefaultRoomService>();
+
+            // Use in-memory database for quick dev and testing 
+            // TODO: Swap out for real database in production
+            services.AddDbContext<HotelApiDbContext>(
+                options => options.UseInMemoryDatabase("landondb"));
+
             services
                 .AddMvc(options=> 
                 {
                     options.Filters.Add<JsonExceptionFilter>();
+                    options.Filters.Add<RequireHttpsOrCloseAttribute>();
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             
@@ -44,6 +60,19 @@ namespace LandonApi
                 options.ReportApiVersions = true;
                 options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
             });
+
+            //allows any api with different origin (for example http call from https://example.com to api https://api.example.com)
+            services.AddCors(options =>
+            {
+                //for test purposes only, allows any origin
+                options.AddPolicy("AllowAnyApp", policy => policy.AllowAnyOrigin());
+
+                //can be used in production
+                //options.AddPolicy("AllowAnyApp", policy => policy.WithOrigins("https://example.com"));
+            });
+
+            services.AddAutoMapper(
+                options => options.AddProfile<MappingProfile>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,7 +87,11 @@ namespace LandonApi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //Commented as we use RequireHttpsOrCloseAttribute
+            //app.UseHttpsRedirection();
+
+            app.UseCors("AllowAnyApp");
+
             app.UseMvc();
         }
     }
